97af95b3820f2e7c4b1eb73fb9249651d4be0c9f
Jamal077011 <jamal@sketch-tech.com>
2023-09-06 12:50:42 +0300

    new

--- resources/views/frontend/dashboard/pages/sponsoreFile/fileForm.blade.php
+++ resources/views/frontend/dashboard/pages/sponsoreFile/fileForm.blade.php
@@ -63,11 +63,11 @@
 <script>
     $('#start_date').datepicker({
         uiLibrary: 'bootstrap4',
-        format: "dd-mm-yyyy",
+        format: "yyyy-mm-dd",
     });
     $('#end_date').datepicker({
         uiLibrary: 'bootstrap4',
-        format: "dd-mm-yyyy",
+        format: "yyyy-mm-dd",
     });
 </script>
 @endsection
